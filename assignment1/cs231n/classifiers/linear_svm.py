b008ea73e695f32db04ca71ad09e96ce716c07d8
Canh Do Minh <minhcanh99@gmail.com>
2020-01-10 00:41:27 +0900

    Finish linear classification with SVM loss function

--- assignment1/cs231n/classifiers/linear_svm.py
+++ assignment1/cs231n/classifiers/linear_svm.py
@@ -36,6 +36,8 @@ def svm_loss_naive(W, X, y, reg):
             margin = scores[j] - correct_class_score + 1 # note delta = 1
             if margin > 0:
                 loss += margin
+                dW[:, j] += X[i]
+                dW[:, y[i]] -= X[i]
 
     # Right now the loss is a sum over all training examples, but we want it
     # to be an average instead so we divide by num_train.
@@ -54,31 +56,33 @@ def svm_loss_naive(W, X, y, reg):
     #############################################################################
     # *****START OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
 
+    # if you want to do a numerical gradient : approximate, slow, easy to write
     # An epsilon number
-    epsilon = 1e-5
-    for r in range(W.shape[0]):
-        for c in range(W.shape[1]):
-            # Adjust a weight value by adding an epsilon (a very very small number) and then calculate gradient
-            hW = W
-            hW[r, c] = hW[r, c] + epsilon
-            for i in range(num_train):
-                hscores = X[i].dot(hW)
-                hcorrect_class_score = hscores[y[i]]
-                for j in range(num_classes):
-                    if j == y[i]:
-                        continue
-                    hmargin = hscores[j] - hcorrect_class_score + 1 # note delta = 1
-                    if hmargin > 0:
-                        dW[r, c] += hmargin
-            # Calculate an average by dividing by num_train
-            dW[r, c] /= num_train
-            # Add regularization to the lost
-            dW[r, c] += reg * np.sum(hW * hW)
-            # reset again
-            hW[r, c] = hW[r, c] - epsilon
-
+    # epsilon = 1e-5
+    # for r in range(W.shape[0]):
+    #     for c in range(W.shape[1]):
+    #         # Adjust a weight value by adding an epsilon (a very very small number) and then calculate gradient
+    #         hW = W
+    #         hW[r, c] = hW[r, c] + epsilon
+    #         for i in range(num_train):
+    #             hscores = X[i].dot(hW)
+    #             hcorrect_class_score = hscores[y[i]]
+    #             for j in range(num_classes):
+    #                 if j == y[i]:
+    #                     continue
+    #                 hmargin = hscores[j] - hcorrect_class_score + 1 # note delta = 1
+    #                 if hmargin > 0:
+    #                     dW[r, c] += hmargin
+    #         # Calculate an average by dividing by num_train
+    #         dW[r, c] /= num_train
+    #         # Add regularization to the lost
+    #         dW[r, c] += reg * np.sum(hW * hW)
+    #         # reset again
+    #         hW[r, c] = hW[r, c] - epsilon
     # Finalize the derivative of the loss function
-    dW = (dW - loss) / epsilon
+    # dW = (dW - loss) / epsilon
+
+    dW /= num_train
 
     # *****END OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
     
@@ -94,6 +98,7 @@ def svm_loss_vectorized(W, X, y, reg):
     """
     loss = 0.0
     dW = np.zeros(W.shape) # initialize the gradient as zero
+    num_train = X.shape[0]
 
     #############################################################################
     # TODO:                                                                     #
@@ -102,7 +107,12 @@ def svm_loss_vectorized(W, X, y, reg):
     #############################################################################
     # *****START OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
 
-    pass
+    scores = X.dot(W)
+    yi_scores = scores[np.arange(num_train), y]
+    margins = np.maximum(0, scores - yi_scores.reshape(1, num_train).T + 1)
+    margins[np.arange(num_train), y] = 0
+    loss = np.mean(np.sum(margins, axis=1))
+    loss += reg * np.sum(W * W)
 
     # *****END OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
 
@@ -117,7 +127,11 @@ def svm_loss_vectorized(W, X, y, reg):
     #############################################################################
     # *****START OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
 
-    pass
+    binary = margins
+    binary[margins > 0] = 1
+    row_sum = np.sum(binary, axis=1)
+    binary[np.arange(num_train), y] = -row_sum.T
+    dW = np.dot(X.T, binary) / num_train
 
     # *****END OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
 
